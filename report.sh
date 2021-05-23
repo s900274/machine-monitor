ea8fe0cd2ca60fefc3f69640e82b04b85e1d4b99
Jeff Wang <jeff.wang@ruckuswireless.com>
2021-05-23 20:20:36 +0800

    change ip for Jeff-ThinkPad-P52s

--- report.sh
+++ report.sh
@@ -1,7 +1,7 @@
-sleep 30 #wait for interfaces up
+##sleep 30 #wait for interfaces up
 HOST=`hostname`
+cd $1
 git pull --no-edit
-cd /home/isaac/projects/mymachines
 echo "`ip addr | grep inet`" > $HOST.txt
 echo "`date`" >> $HOME/report.log
 git add .
